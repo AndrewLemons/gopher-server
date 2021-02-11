53d7342459739145de231b1ddd24a305a6b74bb8
Andrew <andrewglemons@gmail.com>
2021-02-10 20:13:58 -0500

    Add more test routes

--- test/server.js
+++ test/server.js
@@ -15,7 +15,15 @@ app.use(new URLRouter());
 // Use a static router
 app.use(new StaticRouter(path.join(__dirname, "./static")));
 
-// Use a Dynamic Router
+// Use a dynamic routers
+app.use(
+	new DynamicRouter("/test", (request, params) => {
+		request.send(`
+		This is a dynamic test page.
+		To test more, make a request to /test/{anything}.
+		`);
+	})
+);
 app.use(
 	new DynamicRouter("/test/:id", (request, params) => {
 		request.send(`You sent an ID of ${params.id}.`);
